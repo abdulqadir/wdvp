7ff9b75a138080cade2182174b3700897babf4b2
Abdulqadir Rashik <abdulqadir.rashik@gmail.com>
2019-01-11 15:22:35 +0530

    Use data directly fetched from sources

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var lhdi = function(c) { return c['human development index'] < 0.55; };
-var mhdi = function(c) { return c['human development index'] >= 0.55 && c['human development index'] < 0.7; };
-var hhdi = function(c) { return c['human development index'] >= 0.7  && c['human development index'] < 0.8; };
-var vhdi = function(c) { return c['human development index'] >= 0.8; };
+var lhdi = function(c) { return c['hdi'] < 0.55; };
+var mhdi = function(c) { return c['hdi'] >= 0.55 && c['hdi'] < 0.7; };
+var hhdi = function(c) { return c['hdi'] >= 0.7  && c['hdi'] < 0.8; };
+var vhdi = function(c) { return c['hdi'] >= 0.8; };
 var classifyHDI = function(d) {
     if (lhdi(d)) {
         return 'lhdi';
@@ -18,7 +18,7 @@ var classifyHDI = function(d) {
     return 'nodata';
 }
 
-var ddata = _.sortBy(_.filter(data, function(c){return isFinite(c['human development index']);}), function(c){return Number(c['human development index']);});
+var data = _.sortBy(data, function(c){return c['hdi'];});
 
 var dumbbell = function(x, y1, y2) {
     var plotdata = _.filter(data, function(c){return isFinite(c[x]) && isFinite(c[y1]) && isFinite(c[y2]);})
@@ -41,7 +41,7 @@ var dumbbell = function(x, y1, y2) {
             var xscale = this.xscale;
             var yscale = this.yscale;
             var g = elem.selectAll('g').data(plotdata).enter().append('g')
-                .attr('data-country', function(d){return d['indicator'];})
+                .attr('data-country', function(d){return d['country'];})
                 .attr('data-x', function(d) {return d[x];})
                 .attr('data-y1', function(d) {return d[y1];})
                 .attr('data-y2', function(d) {return d[y2];})
@@ -71,7 +71,7 @@ var dumbbell = function(x, y1, y2) {
         }
     }
 }
-var p1 = dumbbell('human development index', 'health expenditure \n% of GDP', 'education expenditure\n% of GDP');
+var p1 = dumbbell('hdi', 'health', 'education');
 p1.draw(d3.select('#plot'));
 
 var plot = function(x, y) {
@@ -97,7 +97,7 @@ var plot = function(x, y) {
                 .attr('class','vhdi').attr('opacity',0.1);
             elem.append('line').attr('x1',0).attr('x2',1140).attr('y1',yscale(0)).attr('y2',yscale(0));
             var g = elem.selectAll('g').data(plotdata).enter().append('g')
-                .attr('data-country', function(d){return d['indicator'];})
+                .attr('data-country', function(d){return d['country'];})
                 .attr('data-x', function(d) {return d[x];})
                 .attr('data-y', function(d) {return d[y];})
             g.append('circle')
@@ -112,13 +112,13 @@ var plot = function(x, y) {
     }
 }
 var s;
-s = plot('human development index', 'political stability & absence of violence');
+s = plot('hdi', 'political stability & absence of violence');
 s.draw(d3.select('#s1'));
-s = plot('human development index', 'government effectiveness');
+s = plot('hdi', 'government effectiveness');
 s.draw(d3.select('#s2'));
-s = plot('human development index', 'regulatory quality');
+s = plot('hdi', 'regulatory quality');
 s.draw(d3.select('#s3'));
-s = plot('human development index', 'rule of law');
+s = plot('hdi', 'rule of law');
 s.draw(d3.select('#s4'));
 
 var htmlNode = d3.select('html').node();
@@ -126,25 +126,23 @@ var countryInfoElem = d3.select('#countryInfo');
 var centerTransform = 'translate(570, 530)';
 
 var arc = d3.arc();
-var arcLength = Math.PI * 2 / ddata.length;
+var arcLength = Math.PI * 2 / data.length;
 var circular = d3.select('#circular').attr('width','1140').attr('height','1052');
 
 var gbackground = circular.append('g').attr('transform',centerTransform);
 arc.outerRadius(450).innerRadius(200);
-gbackground.selectAll('path').data(ddata).enter().append('path').attr('d', function(d, i) {
+gbackground.selectAll('path').data(data).enter().append('path').attr('d', function(d, i) {
     arc.startAngle(i * arcLength).endAngle((i * arcLength) + (arcLength));
     return arc();
 })
 .attr('class', function(d) { return classifyHDI(d) + ' background'; })
-.attr('data-country', function(d){return d['indicator'];})
+.attr('data-country', function(d){return d['country'];})
 .style('opacity',0.1)
 .on('mouseover', function(d) {
-    var country = d['indicator'];
     d3.select(this).transition().duration(100).style('opacity','0.21');
-    d3.selectAll('.background:not([data-country="' + country + '"])').transition().duration(210).style('opacity','0.07');
+    d3.selectAll('.background:not([data-country="' + d['country'] + '"])').transition().duration(210).style('opacity','0.07');
     var mouse = d3.mouse(htmlNode);
     countryInfoElem.style('top', mouse[1]+'px').style('left', (mouse[0]+11)+'px').style('opacity', 0.97);
-    countryInfoElem.select('.card-title').text(country);
 })
 .on('mousemove', function(e) {
     var mouse = d3.mouse(htmlNode);
@@ -157,7 +155,7 @@ gbackground.selectAll('path').data(ddata).enter().append('path').attr('d', funct
 
 var ginner = circular.append('g').attr('transform',centerTransform);
 arc.outerRadius(190).innerRadius(180);
-ginner.selectAll('path').data(ddata).enter().append('path').attr('d', function(d, i) {
+ginner.selectAll('path').data(data).enter().append('path').attr('d', function(d, i) {
     arc.startAngle(i * arcLength).endAngle((i * arcLength) + (arcLength));
     return arc();
 })
@@ -185,13 +183,13 @@ var circularPlot = function(y, outerRadius) {
             for (var s=0; s<stops.length; s++) {
                 var v = stops[s];
                 arc.outerRadius(outerRadius).innerRadius(outerRadius - 3);
-                g.selectAll('.v-' + String(v).replace('.','')).data(ddata).enter().append('path').attr('d', function(d,i) {
+                g.selectAll('.v-' + String(v).replace('.','')).data(data).enter().append('path').attr('d', function(d,i) {
                     if ((s < (stops.length/2) && Number(d[y]) > v) || (s >= stops.length/2 && Number(d[y]) < v)) {
                         arc.startAngle(i * arcLength + (0.15*arcLength)).endAngle((i * arcLength) + (0.8 * arcLength));
                         return arc();
                     }
                 })
-                .attr('data-country', function(d){return d['indicator'];})
+                .attr('data-country', function(d){return d['country'];})
                 .attr('data-y', function(d) {return d[y];})
                 .style('fill', function(d,i) {
                     return colorscale(d[y]);
@@ -212,8 +210,8 @@ c = circularPlot('rule of law', 300);
 c.draw(circular);
 
 var radialLineChart = function(y1, y2, outerRadius, class1, class2) {
-    var validy1 = _.filter(data, function(c){return isFinite(c[y1]);});
-    var validy2 = _.filter(data, function(c){return isFinite(c[y2]);});
+    var validy1 = _.filter(data, function(c){return c[y1] !== null;});
+    var validy2 = _.filter(data, function(c){return c[y2] !== null;});
     var yscale = d3.scaleLinear().domain([0,_.max([
         Number(_.max(validy1, function(c){return Number(c[y1]);})[y1]),
         Number(_.max(validy2, function(c){return Number(c[y2]);})[y2])
@@ -236,18 +234,18 @@ var radialLineChart = function(y1, y2, outerRadius, class1, class2) {
                         return i * arcLength + (0.53 * arcLength);
                     })
                     .radius(function(d,i) {
-                        if (!isFinite(d[y])) {
+                        if (d[y] == null) {
                             return outerRadius-50;
                         }
                         var r = yscale(d[y]);
                         return r;
                     });
             }
-            g.append('path').attr('d', loop(y1)(ddata)).attr('class', class1);
-            g.append('path').attr('d', loop(y2)(ddata)).attr('class', class2);
+            g.append('path').attr('d', loop(y1)(data)).attr('class', class1);
+            g.append('path').attr('d', loop(y2)(data)).attr('class', class2);
         }
     }
 }
 
-var r = radialLineChart('health expenditure \n% of GDP', 'education expenditure\n% of GDP', 250, 'healthexp', 'eduexp');
+var r = radialLineChart('health', 'education', 250, 'healthexp', 'eduexp');
 r.draw(circular);
